bc2846958455cdf952e5f0e026c52e3086e88c3c
Oussamaef <96552812+Oussamaef@users.noreply.github.com>
2023-06-07 23:22:12 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top py-3" id="mainNav">
+    <div class="container px-4 px-lg-5">
+        <a class="navbar-brand text-primary" href="index.php">olbert&olbert</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                <li class="nav-item"><a class="nav-link text-primary" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link text-primary" href="#Our Products">Our Products</a></li>
+                <li class="nav-item"><a class="nav-link text-primary" href="contact/Contact.php" target="_blank">Contact Us</a></li>
+                <li class="nav-item"><a class="nav-link text-primary" href="login.php" target="_blank">Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
